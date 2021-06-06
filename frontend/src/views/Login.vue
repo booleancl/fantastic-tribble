c6161f79ff4ac37014cc7f240838139c7586da83
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2021-06-06 02:48:09 -0400

    refactor(frontend-refactor): Characterization tests on frontend prior Solid refactor. Also global alert added with TDD

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -36,6 +36,7 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import { Auth } from '@/firebase'
 
 export default {
@@ -52,13 +53,18 @@ export default {
     }
   },
   methods: {
+    ...mapActions(['setAlert']),
     validate () {
       return this.$refs.form.validate()
     },
     async login () {
       if (this.validate()) {
-        await Auth.signInWithEmailAndPassword(this.email, this.password)
-        this.$router.push({ name: 'Products' })
+        try {
+          await Auth.signInWithEmailAndPassword(this.email, this.password)
+          this.$router.push({ name: 'Products' })
+        } catch (error) {
+          this.setAlert({ message: 'Error al hacer autenticación', type: 'error' })
+        }
       }
     }
   }
